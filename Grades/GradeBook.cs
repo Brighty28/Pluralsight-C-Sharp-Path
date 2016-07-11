74167fbab06092b9e049bb5b075b4dac5660b614
Brighty28 <abright2012@gmail.com>
2016-07-11 11:54:09 +0100

    Grades Book
    Initial check in

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            _grades = new List<float>();
+        }
+
+        private List<float> _grades;
+
+        public void AddGrade(float grade)
+        {
+            _grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics statistics = new GradeStatistics();
+
+            float sum = 0;
+            foreach (var grade in _grades)
+            {
+                statistics.HighestGrade = Math.Max(grade, statistics.HighestGrade);
+                statistics.LowestGrade = Math.Min(grade, statistics.LowestGrade);
+                sum += grade;
+            }
+            statistics.AvgGrade = sum/_grades.Count;
+            return statistics;
+        }
+    }
+}
---
+++ Grades/GradeStatistcis.cs
@@ -0,0 +1,15 @@
+﻿namespace Grades
+{
+    class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+        public float AvgGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
