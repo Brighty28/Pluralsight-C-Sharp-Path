003c090994c900e7bb144ebd2cd5fd4a4a63d2e0
Brighty28 <abright2012@gmail.com>
2016-07-11 15:58:58 +0100

    Grade books
    Added unit testing

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.HighestGrade);
+            result.HighestGrade.Should().Be(90);
+        }
+
+        [TestMethod]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            //Assert.AreEqual(90, result.HighestGrade);
+            result.LowestGrade.Should().Be(10);
+        }
+
+        [TestMethod]
+        public void ComputesAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AvgGrade, 0.01);
+            //result.LowestGrade.Should()..Be(85.16, 0.01);
+        }
+    }
+}
